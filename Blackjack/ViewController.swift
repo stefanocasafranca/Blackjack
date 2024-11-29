b7b62658b13eb1bd3fbdf0cc231aafe6e2c23daa
stefanocasafranca <scasafrancal01@gmail.com>
2024-11-29 16:56:29 -0600

    Initial Commit

---
+++ Blackjack/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Blackjack
+//
+//  Created by Stefano Casafranca on 11/29/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BlackjackTests/BlackjackTests.swift
@@ -0,0 +1,17 @@
+//
+//  BlackjackTests.swift
+//  BlackjackTests
+//
+//  Created by Stefano Casafranca on 11/29/24.
+//
+
+import Testing
+@testable import Blackjack
+
+struct BlackjackTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
